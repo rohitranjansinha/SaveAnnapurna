f5a0c524c665d514abe472f5e79ff2335c94bc1f
rohitranjansinha <sinharohit511@gmail.com>
2019-09-21 15:32:02 +0530

    first commit

--- SaveAnnapurna_New/firstapp/admin.py
+++ SaveAnnapurna_New/firstapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Mess)
+admin.site.register(Mess_location)
+admin.site.register(NGO)
+admin.site.register(NGO_location)
--- SaveAnnapurna_New/firstapp/views.py
+++ SaveAnnapurna_New/firstapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from  django.http import HttpResponse,HttpResponseRedirect
+from .models import *
+from django.urls import reverse
 # Create your views here.
 
-def fun1():
-    print("HelloWorld")
+
+
+def home_view(request,*args,**kwargs):
+    return render(request, 'firstapp/home.html', {})
